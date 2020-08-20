90ace3503c556f38f09a2be99a08e3b3ec02f64c
dvulanov <dan.vulanovic@hotmail.com>
2020-08-20 13:48:39 -0400

    Update kh_install.sh

--- docs/kh_install.sh
+++ docs/kh_install.sh
@@ -50,12 +50,49 @@ sed -e 's/ ident/ md5/' \
 chmod 600 /var/lib/pgsql/data/pg_hba.conf
 chown postgres:postgres /var/lib/pgsql/data/pg_hba.conf
 
-sudo -u postgres << EOF
+sudo -u postgres bash << EOF
 echo "This is for ckan_default"
 createuser -S -D -R -P ckan_default
 createdb -O ckan_default ckan_default -E utf-8
 psql -d ckan_default -c "create extension postgis;"
 select PostGIS_version();
+
+echo "This is for datastore_default"
+createuser -S -D -R -P -l datastore_default
+createdb -O ckan_default datastore_default -E utf-8
 EOF
+
+echo "Solr"
+mkdir -p /opt/apps/solr
+useradd -m -d /opt/apps/solr -s /bin/bash solr
+chown -R solr /opt/apps/solr
+
+sudo -u solr bash << EOF
+cd /opt/apps/solr
+wget https://archive.apache.org/dist/lucene/solr/6.5.1/solr-6.5.1.tgz
+tar xzvf solr-6.5.1.tgz
+mv solr-6.5.1 6.5.1  # Rename to the version so we get nice path `/opt/apps/solr/6.5.1`
+rm solr-6.5.1.tgz  # Remove the downloaded archive
+EOF
+
+sudo echo "
+[Unit]
+Description=Apache Solr
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/opt/apps/solr/6.5.1/bin/solr start
+ExecReload=/opt/apps/solr/6.5.1/bin/solr restart
+ExecStop=/opt/apps/solr/6.5.1/bin/solr stop
+User=solr
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/solr.service
+sudo chmod 664 /etc/systemd/system/solr.service
+
+sudo systemctl enable solr
+sudo systemctl start solr
 
 . /usr/lib/ckan/default/bin/activate
