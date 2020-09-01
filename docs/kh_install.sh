c2ad885cfe1e193080f4c977754a8ef5e33373c3
dvulanov <dan.vulanovic@hotmail.com>
2020-09-01 15:19:42 -0400

    Update kh_install.sh

--- docs/kh_install.sh
+++ docs/kh_install.sh
@@ -406,7 +406,6 @@ git clone -b 0.0.16 https://github.com/ckan/datapusher.git
 cd datapusher
 pip install -r requirements.txt
 python setup.py develop
-
 deactivate
 EOF
 
@@ -474,5 +473,84 @@ deactivate
 EOF
 
 echo "Config Workers"
+mkdir /var/log/ckan
+chown -R ckan /var/log/ckan
+
+#####
+su -s /bin/bash - ckan << EOF
+
+echo "0 0 * * * /usr/lib/ckan/default/bin/knowledgehub -c /etc/ckan/default/production.ini predictive_search train >/var/log/ckan/cronjob_predictive_search.log 2>&1" | crontab -
+crontab -l > mytempcron
+#echo new cron into cron file
+echo "0 0 * * * /usr/lib/ckan/default/bin/knowledgehub -c /etc/ckan/default/production.ini intents update >/var/log/ckan/cronjob_intents_update.log 2>&1" >> mycron
+#install new cron file
+crontab mytempcron
+rm mytempcron
+
+EOF
+
+echo "Default Worker"
+echo "
+[Unit]
+Description=CKAN Worker Default
+After=network.target
+
+[Service]
+WorkingDirectory=/usr/lib/ckan/default/src/ckan
+ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker --config=/etc/ckan/default/production.ini
+User=ckan
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ckan-worker.default.service
+
+chmod 664 /etc/systemd/system/ckan-worker.default.service
+
+systemctl enable ckan-worker.default
+systemctl start ckan-worker.default
+systemctl status ckan-worker.default
+
+echo "Priority Worker"
+echo "
+[Unit]
+Description=CKAN Worker Priority
+After=network.target
+
+[Service]
+WorkingDirectory=/usr/lib/ckan/default/src/ckan
+ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker priority --config=/etc/ckan/default/production.ini
+User=ckan
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ckan-worker.priority.service
+
+chmod 664 /etc/systemd/system/ckan-worker.priority.service
+
+systemctl enable ckan-worker.priority
+systemctl start ckan-worker.priority
+systemctl status ckan-worker.priority
+
+echo "Bulk Worker"
+
+echo "
+[Unit]
+Description=CKAN Worker Bulk
+After=network.target
+
+[Service]
+WorkingDirectory=/usr/lib/ckan/default/src/ckan
+ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker bulk --config=/etc/ckan/default/production.ini
+User=ckan
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ckan-worker.bulk.service
+
+chmod 664 /etc/systemd/system/ckan-worker.bulk.service
+
+systemctl enable ckan-worker.bulk
+systemctl start ckan-worker.bulk
+systemctl status ckan-worker.bulk
 
 
