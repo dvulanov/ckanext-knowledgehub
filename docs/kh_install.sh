1cd8c72d5b41fb1fa7b911c05fae5b03a728962b
dvulanov <dan.vulanovic@hotmail.com>
2020-09-08 08:07:19 -0400

    Update kh_install.sh

--- docs/kh_install.sh
+++ docs/kh_install.sh
@@ -7,8 +7,11 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-echo "Welcome to my amazing script that does awesomeness and creates Knowledge Hub"
-echo "Base pkg install ..."
+echo "
+############################################
+Part 1: Base Install
+############################################
+"
 yum -y update
 yum install -y epel-release
 yum install -y python-devel postgresql-server postgresql-contrib python-pip python-virtualenv postgresql-devel git redis postgis wget lsof policycoreutils-python java-1.8.0-openjdk
@@ -45,16 +48,11 @@ restorecon -R /usr/lib/ckan/data
 su -s /bin/bash - ckan << EOF
 cd /usr/lib/ckan/
 virtualenv --no-site-packages default
-
 . /usr/lib/ckan/default/bin/activate
-
 echo "Work on python env"
 pip install setuptools==36.1
-
 pip install -e 'git+https://github.com/ckan/ckan.git@ckan-2.8.2#egg=ckan'
-
 pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt
-
 deactivate
 EOF
 
@@ -102,14 +100,12 @@ echo "
 [Unit]
 Description=Apache Solr
 After=network.target
-
 [Service]
 Type=forking
 ExecStart=/opt/apps/solr/6.5.1/bin/solr start
 ExecReload=/opt/apps/solr/6.5.1/bin/solr restart
 ExecStop=/opt/apps/solr/6.5.1/bin/solr stop
 User=solr
-
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/solr.service
@@ -135,63 +131,75 @@ chown -R ckan /etc/ckan/
 #####
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
-
 paster make-config ckan /etc/ckan/default/production.ini
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.orig
-
 sed -e 's/sqlalchemy.url = postgresql:\/\/ckan_default:pass@localhost\/ckan_default/sqlalchemy.url = postgresql:\/\/ckan_default:password@localhost\/ckan_default?sslmode=disable/' \
     -e 's/ckan.site_url =/ckan.site_url = http:\/\/knowledgehub.unhcr.org/' \
     -e 's/ckan.site_id = default/ckan.site_id = unhcr_knowledgehub/' \
     -e 's/#ckan.datastore.write_url/ckan.datastore.write_url/' \
     -e 's/#ckan.datastore.read_url/ckan.datastore.read_url/' \
     -e 's/#solr_url = http:\/\/127.0.0.1:8983\/solr/solr_url = http:\/\/127.0.0.1:8983\/solr\/ckan/' /etc/ckan/default/production.ini.orig > /etc/ckan/default/production.ini 
-
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
 sed -e 's/_default:pass@/_default:password@/' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini 
-
 ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
-
 deactivate
 EOF
 
 #####
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
-
 cd /usr/lib/ckan/default/src/ckan
 paster db init -c /etc/ckan/default/production.ini
 paster --plugin=ckan datastore set-permissions -c /etc/ckan/default/production.ini | psql -h localhost -U ckan_default -d ckan_default
-
 deactivate
 EOF
 
-#####
-echo "Installing ckan Validation extensions"
+echo "
+############################################
+Part 2: Validation Extention
+############################################
+"
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
-
 pip install --no-cache-dir -e "git+https://github.com/frictionlessdata/ckanext-validation.git#egg=ckanext-validation"
 cd /usr/lib/ckan/default/
 pip install -r src/ckanext-validation/requirements.txt
 cd src/ckanext-validation/
-
 paster validation init-db -c /etc/ckan/default/production.ini
+
+mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
+awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation stats"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
 
 deactivate
 EOF
 
-#####
-echo "Installing ckan Data Request OAuth2 extensions"
+echo "
+############################################
+Part 3: Data Requests Extention
+############################################
+"
+su -s /bin/bash - ckan << EOF
+. /usr/lib/ckan/default/bin/activate
+pip install --no-cache-dir -e "git+https://github.com/keitaroinc/ckanext-datarequests.git@kh_stable#egg=ckanext-datarequests"
+
+mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
+awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation stats datarequests"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
+
+pip install humanize==1.0.0
+
+deactivate
+EOF
+
+echo "
+############################################
+Part 4: OAuth2 Extention
+############################################
+"
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
 
-pip install --no-cache-dir -e "git+https://github.com/keitaroinc/ckanext-datarequests.git@kh_stable#egg=ckanext-datarequests"
-pip install humanize==1.0.0
-
 pip install --no-cache-dir -e "git+https://github.com/keitaroinc/ckanext-oauth2.git@kh_stable#egg=ckanext-oauth2"
-
 echo "
-
 # OAuth2 settings
 ckan.oauth2.register_url = https://YOUR_OAUTH_SERVICE/users/sign_up
 ckan.oauth2.reset_url = https://YOUR_OAUTH_SERVICE/users/password/new
@@ -207,56 +215,63 @@ ckan.oauth2.profile_api_user_field = username
 ckan.oauth2.profile_api_fullname_field = full_name
 ckan.oauth2.profile_api_mail_field = email
 ckan.oauth2.authorization_header = Authorization
-
 " >> /etc/ckan/default/production.ini
+
+mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
+awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation stats datarequests oauth2"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
 
 deactivate
 EOF
 
-#####
+echo "
+############################################
+Part 5: Data Store Extention
+############################################
+"
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
 
-pip install --no-cache-dir -e "git+https://github.com/keitaroinc/ckanext-knowledgehub.git#egg=ckanext-knowledgehub"
+mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
+awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation stats datastore datarequests oauth2"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
 
+deactivate
+EOF
+
+echo "
+############################################
+Part 6: Knowledge Hub and Httpd / Nginx config
+############################################
+"
+su -s /bin/bash - ckan << EOF
+. /usr/lib/ckan/default/bin/activate
+pip install --no-cache-dir -e "git+https://github.com/keitaroinc/ckanext-knowledgehub.git#egg=ckanext-knowledgehub"
 # Due to some of the packages requiring newer version of setuptools, we need to uninstall, then reinstall setuptools
 pip uninstall setuptools -y
 pip install setuptools
-
 pip install --no-cache-dir -r "/usr/lib/ckan/default/src/ckanext-knowledgehub/requirements.txt" 
-
 pip uninstall psycopg2-binary -y
 pip uninstall psycopg2 -y
 pip install --no-cache-dir psycopg2==2.7.3.2
-
 python -m spacy download en_core_web_sm
-
 knowledgehub -c /etc/ckan/default/production.ini db init
-
 deactivate
 EOF
 
 #####
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
-
 echo "
-
 # HDX API keys
 ckanext.knowledgehub.hdx.api_key = <HDX_API_KEY>
 ckanext.knowledgehub.hdx.site = test
 ckanext.knowledgehub.hdx.owner_org = <HDX_OWNER_ORG_ID>
 ckanext.knowledgehub.hdx.dataset_source = knowledgehub
 ckanext.knowledgehub.hdx.maintainer = <HDX_USER_NAME>
-
 " >> /etc/ckan/default/production.ini
-
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
 awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation knowledgehub stats datastore datapusher datarequests oauth2"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
-
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
 sed -e 's/#smtp.starttls = False/#smtp.starttls = True/' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini 
-
 deactivate
 EOF
 
@@ -265,7 +280,6 @@ echo "
 import os
 activate_this = os.path.join('/usr/lib/ckan/default/bin/activate_this.py')
 execfile(activate_this, dict(__file__=activate_this))
-
 from paste.deploy import loadapp
 config_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'production.ini')
 from paste.script.util.logging_config import fileConfig
@@ -282,24 +296,18 @@ echo "
     ServerName knowledgehub.prodtest.unhcr.org
     ServerAlias www.knowledgehub.prodtest.unhcr.org
     WSGIScriptAlias / /etc/ckan/default/apache.wsgi
-
     # Pass authorization info on (needed for rest api).
     WSGIPassAuthorization On
-
     # Deploy as a daemon (avoids conflicts between CKAN instances).
     WSGIDaemonProcess ckan_default display-name=ckan_default processes=2 threads=15
-
     WSGIProcessGroup ckan_default
-
     ErrorLog /var/log/httpd/ckan_default.error.log
     CustomLog /var/log/httpd/ckan_default.custom.log combined
-
     <IfModule mod_rpaf.c>
         RPAFenable On
         RPAFsethostname On
         RPAFproxy_ips 127.0.0.1
     </IfModule>
-
     <Directory />
         Require all granted
     </Directory>
@@ -311,8 +319,6 @@ echo "
       AllowOverride All
       Require all granted
     </Directory>
-
-
 </VirtualHost>
 " > /etc/httpd/conf.d/knowledgehub.prodtest.unhcr.org.conf
 
@@ -333,46 +339,36 @@ user nginx;
 worker_processes auto;
 error_log /var/log/nginx/error.log;
 pid /run/nginx.pid;
-
 # Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
 include /usr/share/nginx/modules/*.conf;
-
 events {
     worker_connections 1024;
 }
-
 http {
     log_format  main  '\$remote_addr - \$remote_user [\$time_local] \"\$request\" '
                       '\$status \$body_bytes_sent \"\$http_referer\" '
                       '\"\$http_user_agent\" \"\$http_x_forwarded_for\"';
-
     access_log  /var/log/nginx/access.log  main;
-
     sendfile            on;
     tcp_nopush          on;
     tcp_nodelay         on;
     keepalive_timeout   65;
     types_hash_max_size 2048;
-
     include             /etc/nginx/mime.types;
     default_type        application/octet-stream;
-
     # Load modular configuration files from the /etc/nginx/conf.d directory.
     # See http://nginx.org/en/docs/ngx_core_module.html#include
     # for more information.
     include /etc/nginx/conf.d/*.conf;
-
     # Load enabled sites.
     include /etc/nginx/sites-enabled/*;
     server_names_hash_bucket_size 64;
-
 }
 " > /etc/nginx/nginx.conf
 
 echo "
 proxy_cache_path /tmp/nginx_cache levels=1:2 keys_zone=cache:30m max_size=250m;
 proxy_temp_path /tmp/nginx_proxy 1 2;
-
 server {
     client_max_body_size 100M;
     location / {
@@ -387,7 +383,6 @@ server {
         # In emergency comment out line to force caching
         # proxy_ignore_headers X-Accel-Expires Expires Cache-Control;
     }
-
 }
 " > /etc/nginx/sites-available/ckan.conf
 
@@ -409,7 +404,6 @@ virtualenv /usr/lib/ckan/datapusher
 mkdir /usr/lib/ckan/datapusher/src
 cd /usr/lib/ckan/datapusher/src
 git clone -b 0.0.16 https://github.com/ckan/datapusher.git
-
 . /usr/lib/ckan/datapusher/bin/activate
 cd datapusher
 pip install -r requirements.txt
@@ -419,23 +413,16 @@ EOF
 
 echo "
 <VirtualHost 0.0.0.0:8800>
-
     ServerName ckan
-
     # this is our app
     WSGIScriptAlias / /etc/ckan/datapusher.wsgi
-
     # pass authorization info on (needed for rest api)
     WSGIPassAuthorization On
-
     # Deploy as a daemon (avoids conflicts between CKAN instances)
     WSGIDaemonProcess datapusher display-name=demo processes=1 threads=15
-
     WSGIProcessGroup datapusher
-
     ErrorLog /var/log/httpd/datapusher.error.log
     CustomLog /var/log/httpd/datapusher.custom.log combined
-
     <Directory /etc/ckan>
     Options All
     AllowOverride All
@@ -455,10 +442,8 @@ semanage port -a -t http_port_t -p tcp 8800
 #####
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
-
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
 sed -e 's/#ckan.datapusher.url/ckan.datapusher.url/' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini 
-
 deactivate
 EOF
 
@@ -469,24 +454,28 @@ curl http://localhost:8800
 su -s /bin/bash - ckan << EOF
 . /usr/lib/ckan/default/bin/activate
 
-cd /usr/lib/ckan/default/src/ckan
+mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
+awk '/ckan.plugins = / {print "ckan.plugins = recline_view validation stats datastore datapusher datarequests oauth2"; next}1' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini
 
+cd /usr/lib/ckan/default/src/ckan
 # Add the user
 paster --plugin=ckan user add ckan_admin email=ckan_admin@knowledgehb.org -c /etc/ckan/default/production.ini
-
 # Set the user `ckan_admin` as sysadmin
 paster --plugin=ckan sysadmin add ckan_admin -c /etc/ckan/default/production.ini
-
 deactivate
 EOF
 
+echo "
+############################################
+Part 7: Workers Config
+############################################
+"
 echo "Config Workers"
 mkdir /var/log/ckan
 chown -R ckan /var/log/ckan
 
 #####
 su -s /bin/bash - ckan << EOF
-
 echo "0 0 * * * /usr/lib/ckan/default/bin/knowledgehub -c /etc/ckan/default/production.ini predictive_search train >/var/log/ckan/cronjob_predictive_search.log 2>&1" | crontab -
 crontab -l > mytempcron
 #echo new cron into cron file
@@ -494,7 +483,6 @@ echo "0 0 * * * /usr/lib/ckan/default/bin/knowledgehub -c /etc/ckan/default/prod
 #install new cron file
 crontab mytempcron
 rm mytempcron
-
 EOF
 
 echo "Default Worker"
@@ -502,12 +490,10 @@ echo "
 [Unit]
 Description=CKAN Worker Default
 After=network.target
-
 [Service]
 WorkingDirectory=/usr/lib/ckan/default/src/ckan
 ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker --config=/etc/ckan/default/production.ini
 User=ckan
-
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/ckan-worker.default.service
@@ -523,12 +509,10 @@ echo "
 [Unit]
 Description=CKAN Worker Priority
 After=network.target
-
 [Service]
 WorkingDirectory=/usr/lib/ckan/default/src/ckan
 ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker priority --config=/etc/ckan/default/production.ini
 User=ckan
-
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/ckan-worker.priority.service
@@ -545,12 +529,10 @@ echo "
 [Unit]
 Description=CKAN Worker Bulk
 After=network.target
-
 [Service]
 WorkingDirectory=/usr/lib/ckan/default/src/ckan
 ExecStart=/usr/lib/ckan/default/bin/paster --plugin=ckan jobs worker bulk --config=/etc/ckan/default/production.ini
 User=ckan
-
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/ckan-worker.bulk.service
@@ -560,5 +542,3 @@ chmod 664 /etc/systemd/system/ckan-worker.bulk.service
 systemctl enable ckan-worker.bulk
 systemctl start ckan-worker.bulk
 systemctl status ckan-worker.bulk
-
-
