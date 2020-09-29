80686995e572abe4227fb7bfdf9f6366c6490b1f
dvulanov <dan.vulanovic@hotmail.com>
2020-09-29 11:20:33 -0400

    Update kh_install_prod_centos_singleMachine.sh

--- docs/kh_install_prod_centos_singleMachine.sh
+++ docs/kh_install_prod_centos_singleMachine.sh
@@ -140,7 +140,7 @@ sed -e 's/sqlalchemy.url = postgresql:\/\/ckan_default:pass@localhost\/ckan_defa
     -e 's/ckan.site_id = default/ckan.site_id = unhcr_knowledgehub/' \
     -e 's/#ckan.datastore.write_url/ckan.datastore.write_url/' \
     -e 's/#ckan.datastore.read_url/ckan.datastore.read_url/' \
-    -e 's/#ckan.storage_path/ckan.storage_path/' \
+    -e 's/#ckan.storage_path = \/var\/lib\/ckan/ckan.storage_path = \/var\/lib\/ckan\/data/' \
     -e 's/#solr_url = http:\/\/127.0.0.1:8983\/solr/solr_url = http:\/\/127.0.0.1:8983\/solr\/ckan/' /etc/ckan/default/production.ini.orig > /etc/ckan/default/production.ini 
 mv /etc/ckan/default/production.ini /etc/ckan/default/production.ini.bak
 sed -e 's/_default:pass@/_default:password@/' /etc/ckan/default/production.ini.bak > /etc/ckan/default/production.ini 
